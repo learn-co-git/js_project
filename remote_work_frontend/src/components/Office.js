3dba7059d6837194269f70cedab6d4d646017150
Neil E <argus.two.2@protonmail.com>
2021-02-07 11:56:18 -0700

    clickable share button

--- remote_work_frontend/src/components/Office.js
+++ remote_work_frontend/src/components/Office.js
@@ -1,1 +1,13 @@
+class Office {
+  constructor(){
+    this.createOffice = new OfficeAdapter
+    newOffice()
+  }
 
+  newOffice() {
+    let button = document.getElementById("share")
+    button.addEventListener('click', function(event) {
+      console.log(event)
+    })
+  }
+}
