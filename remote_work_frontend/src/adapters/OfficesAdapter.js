372db8bcaddcafcc87779156982ebe6f92e27fe7
Neil E <argus.two.2@protonmail.com>
2021-03-06 20:36:42 -0700

    office as instance

--- remote_work_frontend/src/adapters/OfficesAdapter.js
+++ remote_work_frontend/src/adapters/OfficesAdapter.js
@@ -10,11 +10,23 @@ class OfficesAdapter {
       return re.json()
     })
     .then(function(json) {
-      Offices.iterate(json)
+      //Office.iterate(json)
+      // loop through the json
+      // create a new instance for every loop
+      // then call the render method for that instance
+      let div = document.getElementById("offices")
+      let ul = document.createElement('ul')
+      div.appendChild(ul)
+      for (let office of json) {
+        // console.log("office time")
+        // console.log(office)
+        const remoteOffice = new Office(office.id, office.cell, office.location, office.bathrooms, office.peace_index, office.description)
+        remoteOffice.render()
+      }
 
     })
     let getComment = new Comment
     getComment.officeComments()
-    
+
   }
 }
--- remote_work_frontend/src/components/Office.js
+++ remote_work_frontend/src/components/Office.js
@@ -1,5 +1,11 @@
 class Office {
-  constructor() {
+  constructor(id, cell, location, bathrooms, peace, description) {
+    this.id = id
+    this.cell = cell
+    this.location = location
+    this.bathrooms = bathrooms
+    this.peaceIndex = peace
+    this.decsription = description
 
   }
 
@@ -22,5 +28,87 @@ class Office {
          new Validate(event)
       })
     })
+  }
+
+
+ render() {
+    let ul = document.querySelector('ul')
+
+      let column = document.createElement('column')
+      column.id = this.id
+      column.className = "column"
+      ul.appendChild(column)
+      let div = document.createElement('div')
+      div.classList.add('card')
+      let id = document.createElement('p')
+      id.className = "office-id"
+      div.appendChild(id)
+      id.value = this.id
+      let h1 = document.createElement('h1')
+      h1.className = "title"
+      div.appendChild(h1)
+      h1.innerHTML = this.location
+      let h31 = document.createElement('h3')
+      h31.className = "details"
+      div.appendChild(h31)
+      h31.innerHTML = "Cell Signal Strength Score  " + this.cell
+      let h32 = document.createElement('h3')
+      h32.className = "details"
+      div.appendChild(h32)
+      h32.innerHTML = "Restroom Quality Score  " + this.bathrooms
+      let h33 = document.createElement('h3')
+      h33.className = "details"
+      div.appendChild(h33)
+      h33.innerHTML = "Working Environment Quality Score  " + this.peaceIndex
+      let p = document.createElement('p')
+      div.appendChild(p)
+      p.innerHTML = "   " + this.decsription + "   "
+      let commentButton = document.createElement('BUTTON')
+      commentButton.id = this.id
+      commentButton.innerHTML = "Add Comment"
+      div.appendChild(commentButton)
+
+      commentButton.addEventListener('click', (event) => {
+        let submitComment = new Comment
+
+        let p = document.createElement('p')
+        p.style.textDecoration = "underline"
+        p.innerHTML = "Add Comment Below"
+        div.appendChild(p)
+        let input = document.createElement("input")
+          input.setAttribute('type','text')
+          input.setAttribute('name', 'comment')
+        let user_name = document.createElement('p')
+        user_name.innerHTML = "Please enter a User-Name"
+        let input2 = document.createElement("input")
+          input.setAttribute('type','text')
+          input.setAttribute('name', 'user_name')
+        let submit = document.createElement("Button")
+          submit.innerHTML = "Submit Comment"
+          submit.addEventListener('click', (event) => {
+            p.style.display = "none"
+            input.style.display = "none"
+            user_name.style.display = "none"
+            input2.style.display = "none"
+            submit.style.display = "none"
+            submitComment.createComment(event, this.id)
+          })
+          div.appendChild(input)
+          div.appendChild(user_name)
+          div.appendChild(input2)
+          div.appendChild(submit)
+      })
+
+      let comments = document.createElement('h3')
+      comments.style.textDecoration = "underline"
+      comments.id = this.id + "office"
+      comments.innerHTML = "Comments"
+      comments.style.color = "red";
+      div.appendChild(comments)
+
+      column.appendChild(div)
+
+
+    ul.appendChild(document.createElement("br"))
   }
 }
